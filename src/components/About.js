0f0026569e6cf3764844ac47246a9d765d1c0c83
jalajp52-design <jalajp52@gmail.com>
2026-01-10 18:40:03 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -4,8 +4,7 @@ import Spline from '@splinetool/react-spline';
 export default function Home() {
   return (
     <main style={{ width: '100%', height: '100vh' }}>
-     <script type="module" src="https://unpkg.com/@splinetool/viewer@1.12.32/build/spline-viewer.js"></script>
-<spline-viewer url="https://prod.spline.design/hhUiJ1MndKyBZ9kW/scene.splinecode"></spline-viewer>
+      <Spline scene="https://prod.spline.design/WOY9r49JRcCMBHBK/scene.splinecode" />
     </main>
   );
 }
